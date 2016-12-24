70f7abc329fe7213926ac81379011ad58ba70dd9
Thomas Lackemann <tommylackemann@gmail.com>
2016-12-24 13:50:07 -0500

    Initial Commit

---
+++ RNCubeTransition/RNCubeTransition.h
@@ -0,0 +1,13 @@
+//
+//  RNCubeTransition.h
+//  RNCubeTransition
+//
+//  Created by Thomas Lackemann on 12/24/16.
+//  Copyright © 2016 Thomas Lackemann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNCubeTransition : NSObject
+
+@end
---
+++ RNCubeTransition/RNCubeTransition.m
@@ -0,0 +1,13 @@
+//
+//  RNCubeTransition.m
+//  RNCubeTransition
+//
+//  Created by Thomas Lackemann on 12/24/16.
+//  Copyright © 2016 Thomas Lackemann. All rights reserved.
+//
+
+#import "RNCubeTransition.h"
+
+@implementation RNCubeTransition
+
+@end
